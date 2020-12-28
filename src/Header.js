2bcc8df9cb3089fb6db3c9fae161cb0ffb61aac6
sumit <sumit.pradhan65@gmail.com>
2020-12-29 00:14:47 +0530

    finished desktop

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import logo from './logo.svg'
-import './Header.css'
+import React from "react";
+import logo from "./logo.svg";
+import "./Header.css";
+import axios from "axios";
 
 function Header() {
-    return (
-        <div className="header">
-            <img src={logo} className="header__logo"></img>
-            <h1 className="header__legend">Smart. Simple. Elegant</h1>
-            <span className="header__desc">A smart and simple url shortener. Just paste and go...</span>
-            <div className="header__container">
-                <input type="url" className="header__input"></input>
-                <button className="header__button">GET URL</button>
-            </div>
-        </div>
-    )
+  const getUrl = async function (e) {
+    e.preventDefault();
+    const data = await axios({
+      url: "http://localhost:3001/generateUrl",
+      method: "post",
+      data: {
+        url: document.querySelector(".header__input").value,
+      },
+    });
+    console.log(data);
+  };
+  return (
+    <div className="header">
+      <img src={logo} className="header__logo"></img>
+      <h1 className="header__legend">Smart. Simple. Elegant</h1>
+      <span className="header__desc">
+        A smart and simple url shortener. Just paste and go...
+      </span>
+      <div className="header__container">
+        <input type="url" className="header__input"></input>
+        <button className="header__button">GET URL</button>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
