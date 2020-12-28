79ef6273e45b8f2a506063d3e181dea7b8c25aea
sumit <sumit.pradhan65@gmail.com>
2020-12-29 00:00:26 +0530

    finished mobile first

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import logo from './logo.svg'
+import './Header.css'
 
 function Header() {
     return (
         <div className="header">
-            
+            <img src={logo} className="header__logo"></img>
+            <h1 className="header__legend">Smart. Simple. Elegant</h1>
+            <span className="header__desc">A smart and simple url shortener. Just paste and go...</span>
+            <div className="header__container">
+                <input type="url" className="header__input"></input>
+                <button className="header__button">GET URL</button>
+            </div>
         </div>
     )
 }
